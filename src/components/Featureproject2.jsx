c4b5345fc91249c73c5a9e2af7629ff821b8211d
ImNGodmode <bjames.56@icloud.com>
2023-07-18 19:45:47 -0400

    added detail page for mob gaming

--- src/components/Featureproject2.jsx
+++ src/components/Featureproject2.jsx
@@ -11,8 +11,12 @@ function Featureproject2() {
               <img src={process.env.PUBLIC_URL + '/gameDetail.png'} alt="" className='projectPic'/>
               <img src={process.env.PUBLIC_URL + '/cart.png'} alt="" className='projectPic'/>
               <p>
-                This was one of my first solo projects while I was going through bootcamp. It is a very simple game with a static character in the middle of the screen. Zombies spawn randomly at 8 predetermined locations around the screen and move toward the character. You must shoot (click on) the zombie to kill it before it reaches the character in the middle of the screen. While it is basic, and my skills have far surpassed what is shown here, it is still one of my favorite projects. At the time I created this I had only been coding for 4 weeks, and had only worked with html, css, and JavaScript. If you need a quick break from your daily grind give it a quick play below.
+                This project is a moch ecommerce site for selling video games. It utalizes an api to access video game information and display it in a storefront. The project features user authentication (see top left picture) so users can log in and place games in thier cart for purchase later. Each game also has a more information screen for a more in depth description of the game. The application is responcive to screen size at 3 different increments. Two places I would like to expand on this projet are the cart and pagination. Currently the cart is not connected to a payment system like stripe. When you click on check out your cart is cleared but there isn't a transaction. Also the main landing page of the application brings up the first set of games from the api but there is no option to go further by ether continuing to scroll down or going to the next page. I would like to add this feature for browsing games. 
               </p>
+              <div className='projectLinks'>
+                <div className='github'>To see the code on github click <a href='https://github.com/blakejamesdev/fitnessCoaching' target="_blank" rel='noreferrer'>here!</a></div>
+                <div className='github'>To go to a live version of the project click<a href='https://github.com/blakejamesdev/fitnessCoaching' target="_blank" rel='noreferrer'>here!</a>(this project is currently not being hosted)</div>
+              </div>
             </div>
         </div>
         
